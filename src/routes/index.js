b1cb2210f3a2508525abc6ffbf9d8f9ebd086360
nandukalidindi <nandukalidindi@gmail.com>
2018-09-22 17:37:15 -0400

    Prevent the title error when route is undefined for some reason

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,12 +18,12 @@ const routes = {
     {
       path: '',
       load: () => import(/* webpackChunkName: 'home' */ './home'),
-    }
+    },
   ],
 
   async action({ next }) {
     // Execute each child route until one of them return the result
-    const route = await next();
+    const route = (await next()) || {};
 
     // Provide default values for title, description etc.
     route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
